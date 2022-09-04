dd311ae11235fb8944f42ea42e0992ec64fcd96f
Vivek Nagose <nagosevivek19@gmail.com>
2022-09-04 10:02:55 +0530

    This is my first release of ATM program

---
+++ ATM.py
@@ -0,0 +1,16 @@
+print("Insert your Card")
+CorrectPIN = int(1234)
+i=1
+while i<5:
+    PIN=int(input("Enter your PIN - "))
+    if PIN != CorrectPIN:
+        print("Incorrect PIN, Try Again.")
+        print("chances left - " + str(3-i))
+        i+=1
+        if i==4:
+            print("Card blocked")
+            break
+    else:
+        amount = input("Enter amount - ")
+        print(amount)
+    
